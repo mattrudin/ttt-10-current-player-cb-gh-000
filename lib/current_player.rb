2c4bd82a450ae67ecb5d8cbdb1edb8371c935b81
Matthias Rudin <matthias.rudin@live.com>
2018-11-07 18:51:49 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,4 +1,5 @@
 def turn_count(board)
   counter = 0
-  board.each { |slot| slot == "X" || "O" ? counter += 1 : counter += 0}
+  board.each { |slot| slot == "X" || "O" ? counter += 1 : counter += 0 }
+  counter
 end
